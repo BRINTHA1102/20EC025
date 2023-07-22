bd5a0f794958a4b43bdc1906bf312e0d6d314696
BRINTHA1102 <124517155+BRINTHA1102@users.noreply.github.com>
2023-07-22 12:20:03 +0530

    Create train app.js

---
+++ train app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TrainScheduleApp from './components/TrainScheduleApp';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      
+      <TrainScheduleApp />
+    </div>
+  );
+};
+
+export default App;
